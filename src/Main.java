4a43c25c7b4fe0d75a67dd46524d44306430ee30
yasirsaglam <yasirsaglamm@gmail.com>
2022-08-30 22:07:11 +0300

    "Proje Tamamlandı"

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int dogumYil, x;
+        String burc = null;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Doğum yılınızı giriniz : ");
+        dogumYil = input.nextInt();
+        x = dogumYil % 12;
+        switch (x) {
+            case 0:
+                burc = "Maymun";
+                break;
+            case 1:
+                burc = "Horoz";
+                break;
+            case 2:
+                burc = "Köpek";
+                break;
+            case 3:
+                burc = "Domuz";
+                break;
+            case 4:
+                burc = "Fare";
+                break;
+            case 5:
+                burc = "Öküz";
+                break;
+            case 6:
+                burc = "Kaplan";
+                break;
+            case 7:
+                burc = "Tavşan";
+                break;
+            case 8:
+                burc = "Ejderha";
+                break;
+            case 9:
+                burc = "Yılan";
+                break;
+            case 10:
+                burc = "At";
+                break;
+            case 11:
+                burc = "Koyun";
+                break;
+        }
+        System.out.print("Çin Zodyağı Burcunuz : " + burc);
+    }
+}
